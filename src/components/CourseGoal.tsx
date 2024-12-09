74d8e8bd0a9891aaac096ef4a39ed4b8d4050780
Alireza Raei <alirezaraei17@gmail.com>
2024-12-09 15:21:48 +1100

    refactor: Update CourseGoal component to use children prop
    - Modified `CourseGoal` component to accept `children` prop instead of `description`.
    - Updated `App` component to pass description as a child to `CourseGoal`.

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,14 +1,21 @@
+// import { type PropsWithChildren} from "react";
+import { type ReactNode } from "react";
+
+// method 1
 type CourseGoalProps = {
   title: string;
-  description: string;
+  children: ReactNode;
 };
 
-function CourseGoal({ title, description }: CourseGoalProps) {
+// method 2
+// type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>
