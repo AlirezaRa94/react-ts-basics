94a52eaedc571031024448b969d6dd4daf7b5071
Alireza Raei <alirezaraei17@gmail.com>
2024-12-11 16:35:55 +1100

    feat: Add InfoBox component and integrate it into CourseGoalList
    - Created `InfoBox` component to display hints and warnings.
    - Updated `CourseGoalList` to show a hint when there are no goals and a warning when there are more than three goals.
    - Added styles for `InfoBox` in `index.css`.

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,8 @@
+import { type ReactNode } from "react";
+
 import { type CourseGoal as courseGoalType } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 type courseGoalListProps = {
   goals: courseGoalType[];
@@ -7,16 +10,37 @@ type courseGoalListProps = {
 };
 
 function CourseGoalList({ goals, onDeleteGoal }: courseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Why not add one?
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length > 3) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You have a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
